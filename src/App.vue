1bd757947a4cd1d1e442ef60460c300a4719a2e0
martin <martinfrontend@gmail.com>
2024-04-09 23:36:57 +0800

    feat: add sass、九宮格

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HomePage from "./components/HomePage.vue";
+import HamburgerIcon from "./components/icons/IconHamburger.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <button>
+      <HamburgerIcon />
+    </button>
   </header>
 
   <main>
-    <TheWelcome />
+    <HomePage />
   </main>
 </template>
 
 <style scoped>
 header {
+  display: flex;
+  justify-content: end;
+  padding: 10px;
   line-height: 1.5;
+  button {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  background-color: #d6d6d6;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,98 @@
+<script setup></script>
+<template>
+  <div class="container">
+    <div class="cell">
+      <div class="background"></div>
+      <div class="ball"></div>
+    </div>
+    <div class="cell">
+      <div class="background"></div>
+    </div>
+    <div class="cell">
+      <div class="background blink-animate"></div>
+      <div class="ball"></div>
+    </div>
+
+    <div class="cell">
+      <div class="background"></div>
+    </div>
+    <div class="cell">
+      <div class="background blink-animate"></div>
+    </div>
+    <div class="cell">
+      <div class="background"></div>
+    </div>
+
+    <div class="cell">
+      <div class="background"></div>
+      <div class="ball"></div>
+    </div>
+    <div class="cell">
+      <div class="background"></div>
+    </div>
+    <div class="cell">
+      <div class="background blink-animate"></div>
+      <div class="ball"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 100px);
+  gap: 5px;
+  padding: 0 5px;
+  overflow: hidden;
+}
+
+.cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: black solid 2px;
+  position: relative;
+  .background {
+    height: 100%;
+    width: 100%;
+    background: radial-gradient(
+      circle,
+      rgba(113, 81, 95, 1) 81%,
+      rgba(0, 0, 0, 1) 100%
+    );
+    &.blink-animate {
+      animation: blink 2s infinite;
+    }
+  }
+  .ball {
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #a5f12b;
+    animation: move 2s infinite;
+    z-index: 1;
+  }
+}
+
+@keyframes blink {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.6;
+  }
+}
+
+@keyframes move {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(100px);
+  }
+}
+</style>
