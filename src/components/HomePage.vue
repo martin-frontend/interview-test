ff3e831b0ac06df19772382efa51b90fa8d9748d
martin <martinfrontend@gmail.com>
2024-04-10 16:46:04 +0800

    feat: 四顆球同時朝同一個座標點移動

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,38 +1,56 @@
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from "vue";
+const jiugongge = reactive([
+  { key: 1, ball: true, blink: false, button: true },
+  { key: 2, ball: false, blink: false },
+  { key: 3, ball: true, blink: true },
+  { key: 4, ball: false, blink: false },
+  { key: 5, ball: false, blink: true },
+  { key: 6, ball: false, blink: false },
+  { key: 7, ball: true, blink: false },
+  { key: 8, ball: false, blink: false },
+  { key: 9, ball: true, blink: true },
+]);
+
+const handelMove = () => {
+  // 取得所有的球的外部 div 和球的 DOM 元素
+  const cells = document.querySelectorAll(".cell");
+  const ballCells = [];
+  jiugongge.forEach((item, index) => {
+    if (item.ball) ballCells.push(cells[index]);
+  });
+  const balls = document.querySelectorAll(".ball");
+
+  // 定義目標座標
+  const targetX = 150;
+  const targetY = 300;
+
+  // 將每個球移動到目標座標
+  balls.forEach((ball, index) => {
+    ball.style.animation = "none";
+
+    const cell = ballCells[index];
+    const rect = cell.getBoundingClientRect();
+    const currentX = rect.left + window.scrollX;
+    const currentY = rect.top + window.scrollY;
+
+    const deltaX = targetX - currentX;
+    const deltaY = targetY - currentY;
+
+    // 使用 CSS 的 transform 屬性來移動球
+    ball.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+  });
+};
+</script>
+
 <template>
   <div class="container">
-    <div class="cell">
-      <div class="background"></div>
-      <div class="ball"></div>
-    </div>
-    <div class="cell">
-      <div class="background"></div>
-    </div>
-    <div class="cell">
-      <div class="background blink-animate"></div>
-      <div class="ball"></div>
-    </div>
-
-    <div class="cell">
-      <div class="background"></div>
-    </div>
-    <div class="cell">
-      <div class="background blink-animate"></div>
-    </div>
-    <div class="cell">
-      <div class="background"></div>
-    </div>
-
-    <div class="cell">
-      <div class="background"></div>
-      <div class="ball"></div>
-    </div>
-    <div class="cell">
-      <div class="background"></div>
-    </div>
-    <div class="cell">
-      <div class="background blink-animate"></div>
-      <div class="ball"></div>
+    <div class="cell" v-for="item of jiugongge" :key="item.key">
+      <div class="background" :class="{ 'blink-animate': item.blink }"></div>
+      <div v-if="item.ball" class="ball"></div>
+      <button v-if="item.button" class="button" @click="handelMove">
+        讓球集合
+      </button>
     </div>
   </div>
 </template>
@@ -74,6 +92,12 @@
     background-color: #a5f12b;
     animation: move 2s infinite;
     z-index: 1;
+    transition: all 1s;
+  }
+
+  .button {
+    position: absolute;
+    top: 0;
   }
 }
 
